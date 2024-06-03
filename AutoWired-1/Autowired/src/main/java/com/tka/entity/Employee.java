1cd6b38a946853015cf9366ab2615ec31af71e49
Harshwardhan Rajapure <118995657+Harshwardhan-Rajapure@users.noreply.github.com>
2024-06-03 23:39:44 +0530

    Add files via upload

---
+++ AutoWired-1/Autowired/src/main/java/com/tka/entity/Employee.java
@@ -0,0 +1,70 @@
+package com.tka.entity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+
+	//@Autowired
+	@Value("${myid}")
+	int id;
+	
+	@Value("${myname}")
+	String name;
+	
+	@Autowired
+	Country country;
+	
+	public Employee() {
+		
+	}
+	
+//	@Autowired
+//	public Employee(@Value("Harshwardhan") String name) {
+//		super();
+//		this.name = name;
+//	}
+	
+	public Employee(String name, Country country) {
+		super();
+		this.name = name;
+		this.country = country;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public Country getCountry() {
+		return country;
+	}
+
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", country=" + country + "]";
+	}
+
+//	@Override
+//	public String toString() {
+//		return "Employee [id=" + id + ", name=" + name + "]";
+//	}
+	
+}
---
+++ AutoWired-1/Autowired/src/main/java/com/tka/main/MainClass.java
@@ -0,0 +1,20 @@
+package com.tka.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.tka.entity.Employee;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ApplicationContext context = new AnnotationConfigApplicationContext(Configuration.class);
+		
+		Employee emp= context.getBean(Employee.class);
+		
+		System.out.println(emp);
+	}
+
+}
